20f3ea9328b22c13d8d028868cab43478c6e73d2
tranhanh18042 <hanhtran18042@gmail.com>
2022-05-13 13:06:31 +0700

    fix position form login

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/sass/js/feed_back.js', 'public/js')
-    .js('resources/sass/js/login_register.js', 'public/js')
+mix.js('resources/js/feed_back.js', 'public/js')
+    .js('resources/js/login_register.js', 'public/js')
     .vue()
     .sass('resources/sass/HomePage/app.scss', 'public/css')
     .copy('resources/asset/img', 'public/img')

79ac562fefb027188557a42102371e07d9a0ba61
tranhanh18042 <hanhtran18042@gmail.com>
2022-05-17 09:03:26 +0700

    add file js

--- resources/js/login_register.js
+++ resources/js/login_register.js
@@ -1,17 +1,11 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!*********************************************!*\
-  !*** ./resources/scss/js/login_register.js ***!
-  \*********************************************/
 $(document).ready(function () {
   $(".btn-register").click(function () {
-    $(".content-login").css('display', 'none');
-    $(".content-register").css('display', 'block');
+    $(".content-login").css('display', 'none')
+    $(".content-register").css('display', 'block')
   });
+
   $(".btn-login-header").click(function () {
-    $(".content-register").css('display', 'none');
-    $(".content-login").css('display', 'block');
+    $(".content-register").css('display', 'none')
+    $(".content-login").css('display', 'block')
   });
 });
-/******/ })()
-;

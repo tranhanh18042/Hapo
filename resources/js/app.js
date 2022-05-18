382c49e3a51247d0b20ddd4c67fdde51d7453b0f
tranhanh18042 <hanhtran18042@gmail.com>
2022-05-18 17:31:27 +0700

    fix home pgae

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,7 @@
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
-
+ window.$ = window.jQuery = require('jquery')
 require('./bootstrap');
 require('./login_register');
 window.Vue = require('vue').default;
--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,5 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js').vue()
     .sass('resources/sass/app.scss', 'public/css')
     .copy('resources/asset/img','public/img')
+    .copy('node_modules/font-awesome/fonts', 'public/fonts')
     .version();
